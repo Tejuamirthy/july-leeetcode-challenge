7bc2eab91ebfee69937fbb932bf16ed80c47a15d
Tejeshwar Amirthy <tejuamirthi@gmail.com>
2020-07-12 19:17:26 +0530

    Day 11 july leetcode challenge get all subsets

---
+++ Day11.java
@@ -0,0 +1,28 @@
+// Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+// Note: The solution set must not contain duplicate subsets.
+
+
+class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+         List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        if(nums.length == 0){
+            return result;
+        }
+
+        Arrays.sort(nums);
+        dfs(nums, 0, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    public void dfs(int[] s, int index, List<Integer> path, List<List<Integer>> result){
+        result.add(new ArrayList<Integer>(path));
+
+        for(int i = index; i < s.length; i++){
+            path.add(s[i]);
+            dfs(s, i+1, path, result);
+            path.remove(path.size()-1);
+        }
+    }
+}
